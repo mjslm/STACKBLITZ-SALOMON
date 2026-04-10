f4e2bcc14b4b2fc55643312f3b2e69ff63b92209
mjslm <marchaeljadesalomon.202401261@gmail.com>
2026-04-10 10:58:44 +0800

    pogii

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-// run `node index.js` in the terminal
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//dataset
+const students = [
+    {
+        id: 1,
+        name: "Jene",
+        yearLevel: 2,
+    },
+    {
+        id: 2,
+        name: "Quinz",
+        yearLevel: 2,
+    },
+    {
+        id: 3,
+        name: "MamiTine",
+        yearLevel: 2,
+    },
+]
+
+app.get('/api/students', (req, res) => {
+    res.json(students)
+})
+app.post('/api/students', (req, res) => {
+    const {name, yearLevel} = req.body
+    const newStudent = {name, yearLevel}
+    students.push(newStudent)
+
+    res.json(201).json({
+        message: "Student added successfuly",
+        students: newStudent
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+
+})
